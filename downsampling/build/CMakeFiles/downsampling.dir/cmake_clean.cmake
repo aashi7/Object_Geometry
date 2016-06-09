0095a75f942e36945ddc2f13f8befc6e5ee61ac7
Aiden Cullo <culloaiden3@gmail.com>
2016-06-09 12:25:09 -0400

    adding useful pcl modules also .pcd/.ply files

---
+++ downsampling/build/CMakeFiles/downsampling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/downsampling.dir/downsampling.cpp.o"
+  "downsampling.pdb"
+  "downsampling"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/downsampling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
